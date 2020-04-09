824ea88c4caca77079d5f4d4dd72ed54ac11c962
Ellis Chang <whsh320@gmail.com>
2020-04-09 15:08:21 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Streaming Guide
+
+This project is to provide a guide to video watchers where can they watch a specific show / movie.
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import * as XLSX from 'xlsx';
+import { SelectionModel } from '@angular/cdk/collections';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'Streaming Guide';
+  isLoading = false;
+
+  displayedColumns: string[] = ['select','name'];
+  dataSource = new MatTableDataSource(SAMPLE_DATA);
+  selection = new SelectionModel<any>(true, []);
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  ngOnInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  exportExcel() {
+    /* table id is passed over here */
+    let element = document.getElementById('main-data-table');
+    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Exported Data');
+
+    /* save to file */
+    XLSX.writeFile(wb, `${this.title}-${new Date().toISOString().split('T')[0]}.xlsx` );
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: any): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
+}
+
+const SAMPLE_DATA = [{name:'Game of Thrones'}];
