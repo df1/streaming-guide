ff668f0b28f6fd3bda17e95a61536d0b0ee61cf2
Ellis Chang <whsh320@gmail.com>
2020-04-16 00:08:41 -0700

    add sidenav and hamburger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import * as XLSX from 'xlsx';
 import { SelectionModel } from '@angular/cdk/collections';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -21,15 +22,28 @@ export class AppComponent implements OnInit{
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor(
-    private snackBar: MatSnackBar
-  ) {}
+    private snackBar: MatSnackBar,
+    changeDetectorRef: ChangeDetectorRef, 
+    media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
   ngOnInit() {
     this.dataSource.sort = this.sort;
     this.snackBar.open('本網站使用cookies以提昇您的使用體驗及統計。繼續使用本網站表示您同意我們使用cookies。', 
       '同意', { horizontalPosition: 'right'});
   }
+  mobileQuery: MediaQueryList;
 
+  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+
+  private _mobileQueryListener: () => void;
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
   exportExcel() {
     /* table id is passed over here */
     let element = document.getElementById('main-data-table');
