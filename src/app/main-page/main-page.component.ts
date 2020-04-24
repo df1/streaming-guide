eb186c70b79271c445a6f15b6f30beddc7cbc186
Ellis Chang <whsh320@gmail.com>
2020-04-24 11:44:40 -0700

    adjust hero and add SEO

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
+  readonly REPO_URL = '//github.com/df1/streaming-guide';
+  readonly SPONSOR_URL = this.REPO_URL;
+  readonly PROVIDER_LIST = [
+    { id: 1, name: 'Netflix' },
+    { id: 2, name: 'LINE TV' },
+    { id: 3, name: '愛奇藝台灣站' },
+    { id: 4, name: 'HBO GO' },
+    { id: 5, name: 'KKTV' },
+    { id: 6, name: 'Apple TV+' },
+    { id: 7, name: 'GagaOOLala' },
+    { id: 8, name: 'iTunes' },
+    { id: 9, name: 'YouTube' },
+    { id: 10, name: 'Google Play電影' },
+    { id: 11, name: 'CATCHPLAY+' },
+    { id: 12, name: 'friDay影音' },
+    { id: 13, name: 'Hami Video' },
+    { id: 14, name: '公視+' },
+    { id: 15, name: 'LiTV立視線上影視' },
+    { id: 16, name: 'myVideo' },
+    { id: 17, name: 'Vidol影音' },
+    { id: 18, name: 'WeTV' },
+    { id: 19, name: '四季線上影視' },
+    { id: 20, name: '酷瞧' }
+  ];
 
   constructor() { }
 
