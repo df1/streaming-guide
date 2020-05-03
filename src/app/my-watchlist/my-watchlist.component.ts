63d9147d0098949cf43625db427d47abe8a0fcd0
Ellis Chang <whsh320@gmail.com>
2020-05-02 19:11:17 -0700

    menu after login

--- src/app/my-watchlist/my-watchlist.component.ts
+++ src/app/my-watchlist/my-watchlist.component.ts
@@ -10,16 +10,9 @@ import { Router } from '@angular/router';
 export class MyWatchlistComponent implements OnInit {
 
   constructor(
-    private authService: AuthService,
     private router: Router,
   ) { }
 
   ngOnInit(): void {
-  }
-
-  logOut() {
-    this.authService.signOut().subscribe( () => {
-      this.router.navigateByUrl('/');
-    });
   }
 }
