9326d65fb6e0d6bb6489555dbf9efef7ef60a2e5
Ellis Chang <whsh320@gmail.com>
2020-05-11 17:03:29 -0700

    get entity info from firebase

--- src/app/service/entity-resolver.service.ts
+++ src/app/service/entity-resolver.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Router, Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable, from } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { GENRE_MAP } from '../util/constants';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +12,18 @@ export class EntityResolverService implements Resolve<any> {
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
     const entityInfo = this.router.getCurrentNavigation().extras.state;
     if(entityInfo) {
-      return from([entityInfo]).pipe(delay(500));
+      return of(entityInfo);
     }
-    return from([{test:123}]).pipe(delay(1000));
+    return this.afs.collection<any>(route.routeConfig.data.type)
+              .doc(route.params.id).get()
+              .pipe(map(i => ({
+                ...i.data(),
+                genre_ids: i.data().genre_ids.map( genre => GENRE_MAP[genre])
+              })));
   }
 
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    private afs: AngularFirestore,
+  ) {}
 }
